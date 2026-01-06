1840ff216d359d467e6cabc21544f2a57678a150
dimpaulieee <paulandreitimpogjslk0@gmail.com>
2026-01-06 21:29:49 +0800

    Refactor Firebase setup and add exports
    Updated Firebase imports to use specific version URLs and added exports for authentication and Firestore.

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCYTtxWajWspPqkdPggdNHestJ8AYPHto4",
   authDomain: "test-2194f.firebaseapp.com",
   projectId: "test-2194f",
-  storageBucket: "test-2194f.firebasestorage.app",
+  storageBucket: "test-2194f.appspot.com",
   messagingSenderId: "576691817030",
   appId: "1:576691817030:web:85538c0def38c29d0a5847",
   measurementId: "G-ZXDT8R6Q9C"
@@ -18,4 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);      // ← Export auth
+export const db = getFirestore(app);   // ← Export Firestore
+export const analytics = getAnalytics(app);
+
